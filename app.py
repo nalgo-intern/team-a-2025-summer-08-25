668e7035655db55ca53b12dd1e00bad16d5a4b60
Sue Rintaro <sue.rintaro.nalgo.intern@gmail.com>
2025-08-28 11:08:12 +0900

    画像切り取り・OCRへの対応

--- app.py
+++ app.py
@@ -2,11 +2,23 @@
 
 from flask import Flask, request, jsonify, render_template
 from PIL import Image
+import cut_image as ci
+import ocr
+import numpy as np
 
-#import evaluate_statement as es
+import evaluate_statement as es
 import  data_editor as de
 
-#es.get_runtime()
+es.get_runtime()
+lang = "japan"
+ocr_model = ocr._get_ocr(lang)                 # モデル読み込み（重い）
+run_ocr = ocr._make_runner(ocr_model)          # 実行関数確定（分岐を前倒し）
+
+try:
+    dummy = np.zeros((1, 1, 3), dtype=np.uint8)  # 真っ黒 1x1
+    _ = run_ocr(dummy)
+except Exception:
+    pass
 
 app = Flask(__name__)
 
@@ -20,8 +32,8 @@ def analyze():
     data = request.get_json()
     input_text = data['text']
 
-    #model_result = es.analyze_text(input_text)
-    model_result = {'text': 'おはよう<span class="positive">ございます！</span>', 'polarity': {'detail': [{'label': 'negative', 'prob': 0.09024933725595474, 'degree': 'ごく弱い'}, {'label': 'neutral', 'prob': 0.7958078980445862, 'degree': 'かなり'}, {'label': 'positive', 'prob': 0.11394275724887848, 'degree': 'ごく弱い'}], 'pred': {'label': 'neutral', 'prob': 0.7958078980445862, 'degree': 'かなり'}}, 'emotion': {'detail': [{'label': 'joy', 'prob': 0.9646738767623901, 'degree': 'とても'}, {'label': 'anticipation', 'prob': 0.022037865594029427, 'degree': 'ごく弱い'}, {'label': 'surprise', 'prob': 0.004827785771340132, 'degree': 'ごく弱い'}, {'label': 'trust', 'prob': 0.0030676661990582943, 'degree': 'ごく弱い'}, {'label': 'disgust', 'prob': 0.002798834117129445, 'degree': 'ごく弱い'}, {'label': 'sadness', 'prob': 0.0019518053159117699, 'degree': 'ごく弱い'}, {'label': 'anger', 'prob': 0.0018896141555160284, 'degree': 'ごく弱い'}, {'label': 'fear', 'prob': 0.0010431797709316015, 'degree': 'ごく弱い'}]}}
+    model_result = es.analyze_text(input_text)
+    # model_result = {'text': 'おはよう<span class="positive">ございます！</span>', 'polarity': {'detail': [{'label': 'negative', 'prob': 0.09024933725595474, 'degree': 'ごく弱い'}, {'label': 'neutral', 'prob': 0.7958078980445862, 'degree': 'かなり'}, {'label': 'positive', 'prob': 0.11394275724887848, 'degree': 'ごく弱い'}], 'pred': {'label': 'neutral', 'prob': 0.7958078980445862, 'degree': 'かなり'}}, 'emotion': {'detail': [{'label': 'joy', 'prob': 0.9646738767623901, 'degree': 'とても'}, {'label': 'anticipation', 'prob': 0.022037865594029427, 'degree': 'ごく弱い'}, {'label': 'surprise', 'prob': 0.004827785771340132, 'degree': 'ごく弱い'}, {'label': 'trust', 'prob': 0.0030676661990582943, 'degree': 'ごく弱い'}, {'label': 'disgust', 'prob': 0.002798834117129445, 'degree': 'ごく弱い'}, {'label': 'sadness', 'prob': 0.0019518053159117699, 'degree': 'ごく弱い'}, {'label': 'anger', 'prob': 0.0018896141555160284, 'degree': 'ごく弱い'}, {'label': 'fear', 'prob': 0.0010431797709316015, 'degree': 'ごく弱い'}]}}
     updated_result = de.add_emotion_information(model_result)
     filtered_result = de.extract_emotion(updated_result)
     print(filtered_result)
@@ -45,13 +57,14 @@ def analyze_image():
     input_image = Image.open(file.stream)
     
     # --- OCR処理 ---
-    # OCR(input_image) -> extracted_text
+    cuted_image = ci.select_and_crop(input_image)
+    extracted_text = ocr.ocr_from_pil_png(cuted_image, run_ocr=run_ocr)
 
     # ダミーのテキストを返す
-    extracted_text = "画像から抽出したテキストです。今日は良い天気ですね。"
+    # extracted_text = "画像から抽出したテキストです。今日は良い天気ですね。"
 
-    #model_result = es.analyze_text(extracted_text)
-    model_result = {'text': 'おはようございます！', 'polarity': {'detail': [{'label': 'negative', 'prob': 0.09024933725595474, 'degree': 'ごく弱い'}, {'label': 'neutral', 'prob': 0.7958078980445862, 'degree': 'かなり'}, {'label': 'positive', 'prob': 0.11394275724887848, 'degree': 'ごく弱い'}], 'pred': {'label': 'neutral', 'prob': 0.7958078980445862, 'degree': 'かなり'}}, 'emotion': {'detail': [{'label': 'joy', 'prob': 0.9646738767623901, 'degree': 'とても'}, {'label': 'anticipation', 'prob': 0.022037865594029427, 'degree': 'ごく弱い'}, {'label': 'surprise', 'prob': 0.004827785771340132, 'degree': 'ごく弱い'}, {'label': 'trust', 'prob': 0.0030676661990582943, 'degree': 'ごく弱い'}, {'label': 'disgust', 'prob': 0.002798834117129445, 'degree': 'ごく弱い'}, {'label': 'sadness', 'prob': 0.0019518053159117699, 'degree': 'ごく弱い'}, {'label': 'anger', 'prob': 0.0018896141555160284, 'degree': 'ごく弱い'}, {'label': 'fear', 'prob': 0.0010431797709316015, 'degree': 'ごく弱い'}]}}
+    model_result = es.analyze_text(extracted_text)
+    # model_result = {'text': 'おはようございます！', 'polarity': {'detail': [{'label': 'negative', 'prob': 0.09024933725595474, 'degree': 'ごく弱い'}, {'label': 'neutral', 'prob': 0.7958078980445862, 'degree': 'かなり'}, {'label': 'positive', 'prob': 0.11394275724887848, 'degree': 'ごく弱い'}], 'pred': {'label': 'neutral', 'prob': 0.7958078980445862, 'degree': 'かなり'}}, 'emotion': {'detail': [{'label': 'joy', 'prob': 0.9646738767623901, 'degree': 'とても'}, {'label': 'anticipation', 'prob': 0.022037865594029427, 'degree': 'ごく弱い'}, {'label': 'surprise', 'prob': 0.004827785771340132, 'degree': 'ごく弱い'}, {'label': 'trust', 'prob': 0.0030676661990582943, 'degree': 'ごく弱い'}, {'label': 'disgust', 'prob': 0.002798834117129445, 'degree': 'ごく弱い'}, {'label': 'sadness', 'prob': 0.0019518053159117699, 'degree': 'ごく弱い'}, {'label': 'anger', 'prob': 0.0018896141555160284, 'degree': 'ごく弱い'}, {'label': 'fear', 'prob': 0.0010431797709316015, 'degree': 'ごく弱い'}]}}
     updated_result = de.add_emotion_information(model_result)
     filtered_result = de.extract_emotion(updated_result)
     print(filtered_result)
